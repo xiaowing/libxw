f32a378bb22144cadd353737bc911da919f86373
xiaowing <wing0630@gmail.com>
2015-02-28 00:55:56 +0800

    add the CUnit support for the test code

---
+++ test/runtest.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Basic.h"
+#include "CUnit.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    void AddStackTests();
+#ifdef __cplusplus
+}
+#endif
+
+int main(void){
+    if (CU_initialize_registry()){
+        fprintf(stderr, " Initialization of Test Registry failed. ");
+        exit(EXIT_FAILURE);
+    }
+    else{
+        AddStackTests();
+
+        CU_basic_set_mode(CU_BRM_VERBOSE);
+        CU_basic_run_tests();
+    }
+}
--- test/teststack.c
+++ test/teststack.c
@@ -2,23 +2,59 @@
 #include <stdlib.h>
 
 #include "libxw.h"
+#include "CUnit.h"
 
-int main(void){
-    int foo = 12048;
-    int tmp = 0, size=0;
+void test_stack_basic(void){
+    int foo = 12048, bar = 2048;
+    int tmp = 0, size = 0;
 
     LIBXW_MANAGED_STACK *stack = NULL;
     stack = stack_create(NODE_VALUE_INTEGER);
 
     stack_push(stack, NODE_VALUE_INTEGER, &foo, sizeof(int));
 
-    printf("Current count of the stack 0x%p is %d\n", stack, stack_items_counter(stack));
+    CU_ASSERT_EQUAL(stack_items_counter(stack), 1);
 
     stack_peek(stack, NODE_VALUE_INTEGER, &tmp, &size);
 
-    printf("The value of tmp is %d.\n", tmp);
+    CU_ASSERT_EQUAL(tmp, foo);
 
-    stack_pop(stack, NODE_VALUE_INTEGER, &tmp, &size);
+    stack_push(stack, NODE_VALUE_INTEGER, &bar, sizeof(int));
 
-    printf("Current count of the stack 0x%p is %d", stack, stack_items_counter(stack));
+    stack_peek(stack, NODE_VALUE_INTEGER, &tmp, &size);
+
+    CU_ASSERT_EQUAL(tmp, bar);
+
+    stack_dispose(stack);
 }
+
+CU_TestInfo testcase[] = {
+    { "test_stack_basic:", test_stack_basic },
+     CU_TEST_INFO_NULL
+};
+
+int suite_success_init(void){
+    return 0;
+}
+
+int suite_success_clean(void){
+    return 0;
+}
+
+CU_SuiteInfo suites[] = {
+    { "testSuite1", suite_success_init, suite_success_clean, NULL, NULL, testcase },
+    CU_SUITE_INFO_NULL
+};
+
+
+void AddStackTests(){
+    if (NULL != CU_get_registry()){
+        if (!CU_is_test_running()){
+            if (CUE_SUCCESS != CU_register_suites(suites)){
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
+
+
